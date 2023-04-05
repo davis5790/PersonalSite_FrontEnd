a9ab0057788060c8b9c28fffa54172a2fd186415
davis5790 <davis.ne5790@gmail.com>
2023-04-05 16:39:26 -0500

    testing counter

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ function getCount() {
   const url = 'https://7ekl1v31ze.execute-api.us-east-1.amazonaws.com/default/view-counter'
   fetch(url)
   .then(data=>{return data.json()})
-  .then(res=>{console.log(res)})
+  .then(res=>{
+    console.log(res)
+    return res
+  })
 }
 
 
